226fc39b4f85859b3ed27175a2d39ba7fe8db08f
Aj Rejas <73685687+Hamprey01@users.noreply.github.com>
2021-08-25 20:57:16 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,18 +4,21 @@
 int main()
 {
     char c;
-    int upperCase,lowerCase;
+    int upperCase,lowerCase,notUpper,notLower;
     printf("check if a character or letter is a vowel or consonant");
     printf("\nEnter a alphabet:");
     scanf("%c", &c);
 
     lowerCase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     upperCase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    
 
     if (lowerCase || upperCase){
             printf("%c is a vowel", c);
-    }else{
-            printf("%c is a consonant", c);
+    }else if ((c>='a' && c<='z') || (c>='A' && c<='Z')){
+            printf("%c is not a vowel", c);
+    }else {
+         printf("Invalid Input", c);
     }
 
 
